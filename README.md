6863a71fb56b188206ab1c31659d51efbf0e1c49
tomcdj71 <contact.tomc@yahoo.fr>
2023-04-03 18:52:26 +0200

    enh(StringHelper): update 'removeSpecialAndAccent' function for a better slug generation
    PHP's 'intl' extension must be loaded for the function to properly work
    
    function name changed from 'removeSpecialAndAccent' to the better-named 'slugify'

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # MyBlog
+
+## Pre-requesites : 
+---
+PHP 8.2 ('intl' extension enabled)
+Composer
--- src/Helper/StringHelper.php
+++ src/Helper/StringHelper.php
@@ -27,17 +27,35 @@ class StringHelper
     }
 
     /**
-     * Remove special characters and accents.
+     * This function transform a string to a slug.
      *
      * @param string $string
      */
-    public function removeSpecialAndAccent($string): string
+    public function slugify($string): string
     {
-        $string = preg_replace('~[^\pL\d]+~u', '-', $string);
-        $string = iconv('utf-8', 'us-ascii//TRANSLIT', $string);
-        $string = preg_replace('~[^-\w]+~', '', $string);
-        $string = preg_replace('~-+~', '-', $string);
+        // Ensure that the 'intl' extension is loaded.
+        if (!extension_loaded('intl')) {
+            throw new \Exception("The 'intl' extension is required for slug generation.");
+        }
+        // Normalize the string (remove accents, diacritics, etc.).
+        $string = \Normalizer::normalize($string, \Normalizer::FORM_D);
+        if (false === $string) {
+            throw new \Exception('An error occurred while normalizing the string.');
+        }
 
+        // Remove non-letter or non-digit characters, except for spaces.
+        $string = preg_replace('~[^\\pL\d\s]+~u', '', $string);
+        // Replace any spaces with a hyphen.
+        $string = preg_replace('~[\s]+~u', '-', $string);
+        // Convert the string to ASCII, if possible.
+        $string = iconv('utf-8', 'us-ascii//TRANSLIT', $string);
+        // Remove any remaining unwanted characters.
+        $string = preg_replace('~[^-\w]+~', '', $string);
+        // Remove consecutive hyphens.
+        $string = preg_replace('~-+~', '-', $string);
+        // Trim hyphens from the beginning and end of the string.
+        $string = trim($string, '-');
+        // Convert the string to lowercase.
         return strtolower($string);
     }
 }
