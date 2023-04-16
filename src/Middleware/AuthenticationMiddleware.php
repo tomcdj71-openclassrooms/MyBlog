afc8cb774e13860b28ed8a946d5ff9bcc50618bb
tomcdj71 <contact.tomc@yahoo.fr>
2023-04-16 15:33:15 +0200

    feat: add a AuthentcationMiddleware in order to have less code in controller
    This Middleware will be called on all routes that needs to be secured by an user access
    
    functions added : isAdmin  = checks if the user have the role admin ; isUser  = checkes if the user have the role user; isUserOrAdmin = checks if the user have the role admin or the role user
    
    example usage: comment form is locked to connected users, so we will use the new  function to enable the form

---
+++ src/Middleware/AuthenticationMiddleware.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Middleware;
+
+use App\Helper\SecurityHelper;
+
+class AuthenticationMiddleware
+{
+    private $securityHelper;
+
+    public function __construct(SecurityHelper $securityHelper)
+    {
+        $this->securityHelper = $securityHelper;
+    }
+
+    public function __invoke(): void
+    {
+        $user = $this->securityHelper->getUser();
+
+        // if user is not logged in or is not an admin or user
+        if (!$user || !$this->isUserOrAdmin()) {
+            header('Location: /login');
+
+            exit;
+        }
+    }
+
+    public function isAdmin(): bool
+    {
+        $user = $this->securityHelper->getUser();
+        if (!$user || 'ROLE_ADMIN' !== $user->getRole()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function isUser(): bool
+    {
+        $user = $this->securityHelper->getUser();
+        if (!$user || 'ROLE_USER' !== $user->getRole()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function isUserOrAdmin(): bool
+    {
+        $user = $this->securityHelper->getUser();
+        if (!$user || ('ROLE_USER' !== $user->getRole() && 'ROLE_ADMIN' !== $user->getRole())) {
+            return false;
+        }
+
+        return true;
+    }
+}
