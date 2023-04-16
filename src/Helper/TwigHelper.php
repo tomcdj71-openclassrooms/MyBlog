44c6442146bdb7808ce458cefc69961a2af8eea2
tomcdj71 <contact.tomc@yahoo.fr>
2023-04-16 15:27:20 +0200

    enh(TwigHelper): add create asset() and path() functions
    asset function have been created to make the code more clear
    
    path function have been created to have a better link rendering. All links of the views will be updated to use this new function

--- src/Helper/TwigHelper.php
+++ src/Helper/TwigHelper.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Helper;
 
+use App\Router\Route;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 use Twig\TwigFunction;
@@ -21,26 +22,16 @@ class TwigHelper
             'debug' => true,
             'strict_variables' => true,
         ]);
-        $this->twig->addFunction(new TwigFunction('asset', function ($asset) {
-            $protocol = isset($_SERVER['HTTPS']) && 'off' !== $_SERVER['HTTPS'] ? 'https' : 'http';
-            $host = $_SERVER['HTTP_HOST'];
-            $baseURL = "{$protocol}://{$host}";
-
-            return sprintf('%s/assets/%s', $baseURL, ltrim($asset, '/'));
-        }));
+        /*
+         * Add custom functions to Twig.
+         * The above addFunction() refers to the public functions below.
+         */
+        $this->twig->addFunction(new TwigFunction('asset', [$this, 'asset']));
+        $this->twig->addFunction(new TwigFunction('path', [$this, 'path']));
+        $this->twig->addFunction(new TwigFunction('paginate', [$this, 'paginate']));
         // Required for the dump() function
         // VSCode don't like this line but it's working
         $this->twig->addExtension(new \Twig\Extension\DebugExtension());
-    }
-
-    /**
-     * This function takes a path relative to the assets directory.
-     *
-     * @param [string] $path
-     */
-    public function asset_url($path)
-    {
-        return '/assets/'.$path;
     }
 
     /**
@@ -52,5 +43,50 @@ class TwigHelper
     public function render($template, $data = [])
     {
         echo $this->twig->render($template, $data);
+    }
+
+    /**
+     * Generates a URL for a given route.
+     *
+     * @param string $name   The name of the route
+     * @param array  $params An array of parameters to pass to the route
+     *
+     * @return string The generated URL
+     */
+    public function path($name, $params = [])
+    {
+        $route = new Route();
+        $routes = $route->getRoutes();
+
+        if (isset($routes[$name])) {
+            $path = $routes[$name][0];
+
+            return preg_replace_callback('/{([^}]*)}/', function ($match) use ($params) {
+                $param = $match[1];
+                if (isset($params[$param])) {
+                    return $params[$param];
+                }
+
+                return $match[0];
+            }, $path);
+        }
+
+        throw new \Exception(sprintf('Route "%s" not found', $name));
+    }
+
+    /**
+     * Generates an asset URL.
+     *
+     * @param mixed $asset
+     *
+     * @return string The generated URL
+     */
+    public function asset($asset)
+    {
+        $protocol = isset($_SERVER['HTTPS']) && 'off' !== $_SERVER['HTTPS'] ? 'https' : 'http';
+        $host = $_SERVER['HTTP_HOST'];
+        $baseURL = "{$protocol}://{$host}";
+
+        return sprintf('%s/assets/%s', $baseURL, ltrim($asset, '/'));
     }
 }
