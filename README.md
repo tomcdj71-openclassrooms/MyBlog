24e9fb579281e397b356c65893ac9ec5f06b5998
tomcdj71 <contact.tomc@yahoo.fr>
2023-04-24 10:15:59 +0200

    update README.md

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
+[![Codacy Badge](https://app.codacy.com/project/badge/Grade/193fb464761e4d38b5248a686e6fedcc)](https://app.codacy.com/gh/tomcdj71/MyBlog/dashboard?utm_source=gh&utm_medium=referral&utm_content=&utm_campaign=Badge_grade)
 # MyBlog
 
 ## Pre-requesites : 
 ---
-PHP 8.2 ('intl' extension enabled)
-Composer
+- PHP 8.2 ('intl' extension enabled)
+- Composer
+- npm (I used pnpm)
+
+`composer install --no-dev --optimize-autoloader`
+
+then, navigate to public and do :
+`pnpm install`
+
+if you don't have pnpm, run `npm install`
---
+++ public/assets/js/tables.js
@@ -0,0 +1,192 @@
+function getObjectType(row) {
+    if (row.hasOwnProperty('tags')) {
+        return 'post';
+    } else if (row.hasOwnProperty('content')) {
+        return 'comment';
+    } else {
+        return 'unknown';
+    }
+}
+
+function toggleCardView() {
+    if (window.innerWidth < 768) {
+        $('#table-user-profile-posts').bootstrapTable('refreshOptions', { cardView: true });
+        $('#table-user-profile-comments').bootstrapTable('refreshOptions', { cardView: true });
+        $('#table-all-comments').bootstrapTable('refreshOptions', { cardView: true });
+    } else {
+        $('#table-user-profile-posts').bootstrapTable('refreshOptions', { cardView: false });
+        $('#table-user-profile-comments').bootstrapTable('refreshOptions', { cardView: false });
+        $('#table-all-comments').bootstrapTable('refreshOptions', { cardView: false });
+    }
+}
+
+function titleFormatter(value, row, index) {
+    var titleLength = 15;
+    var title = row.post ? row.post.title : row.title;
+    var entityType = getObjectType(row);
+    if (entityType === 'comment') {
+        title = title.slice(0, titleLength);
+    } else if (entityType === 'post') {
+        title = title;
+    }
+    var slug = row.post ? row.post.slug : row.slug;
+    var link = '<a href="/blog/post/' + slug + '">' + title + '</a>';
+    return link;
+}
+
+function tagsFormatter(value, row, index) {
+    var tags = row.post ? row.post.tags : row.tags;
+    var tagsArray = tags.split(', '); // Split the string into an array
+    var tagsLength = 5;
+    var tagsList = tagsArray.slice(0, tagsLength);
+    var tagsListLength = tagsList.length;
+    var tagsListString = '';
+    for (var i = 0; i < tagsListLength; i++) {
+        tagsListString += '<span class="badge bg-info">' + tagsList[i] + '</span> ';
+    }
+    return tagsListString;
+}
+
+function contentPreviewFormatter(value, row, index) {
+    var previewLength = 50;
+    var preview = row.content ? row.content.slice(0, previewLength) : "";
+    return preview + '...';
+}
+
+function dateFormatter(value, row, index) {
+    var date = new Date(row.created_at);
+    return ('0' + date.getDate()).slice(-2) + '/' + (
+        '0' + (
+            date.getMonth() + 1
+        )
+    ).slice(-2) + '/' + date.getFullYear();
+}
+
+function isEnabledFormatter(value, row, index) {
+    var status = row.is_enabled;
+    var entityType = getObjectType(row);
+    if (status == 1) {
+        return '<span class="badge bg-success">' + (entityType === 'post' ? 'Publié' : 'Validé') + '</span>';
+    } else if (status == 0 || status == null) {
+        return '<span class="badge bg-warning">' + (entityType === 'post' ? 'Dépublié' : 'Non validé') + '</span>';
+    }
+}
+
+function userFormatter(value, row, index) {
+    var user = row.user;
+    var userLink = '<a href="/profile/' + user.username + '">' + user.username + '</a>';
+    return userLink;
+}
+
+function actionFormatter(value, row, index) {
+    var entityType = getObjectType(row);
+    var postSlug = row.post ? row.post.slug : row.slug;
+    var commentId = row.id;
+    return '<a href="/blog/post/' + postSlug + (entityType === 'comment' ? '#comment-' + commentId : '') + '" class="btn btn-sm btn-primary"><i class="bi bi-eye"> </i>Voir</a>';
+}
+
+function updateBootstrapTableOptions(page, limit) {
+    $('#table-users').bootstrapTable('refreshOptions', {
+        pageNumber: page,
+        pageSize: limit
+    });
+}
+
+function initBootstrapTable(selector, config) {
+    var $element = $(selector);
+    if ($element.length === 0) {
+        console.warn("Element not found for selector:", selector);
+        return;
+    }
+    if ($element.length > 1) {
+        console.warn("Multiple elements found for selector:", selector);
+        return;
+    }
+    $element.bootstrapTable({
+        ...config,
+        queryParams: function (params) {
+            var currentPage = Math.floor(params.offset / params.limit) + 1;
+            var pageSize = params.limit;
+
+            return {
+                limit: pageSize,
+                offset: params.offset,
+                page: currentPage,
+            };
+        },
+    });
+}
+
+function generateTableConfig(url, columns, onPostBody) {
+    return {
+        url,
+        columns,
+        pagination: true,
+        sidePagination: 'server',
+        pageSize: 10,
+        pageList: [10, 25, 50, 100],
+        search: true,
+        showColumns: true,
+        showRefresh: true,
+        showToggle: true,
+        toggle: 'table',
+        toolbar: '#toolbar',
+        classes: 'table table-hover table-bordered table-sm fixed-height-table table-responsive-md',
+        smartDisplay: true,
+        locale: 'fr-FR',
+        icons: {
+            refresh: 'bi bi-arrow-clockwise',
+            toggle: 'bi bi-list',
+            columns: 'bi bi-columns-gap',
+            paginationSwitchDown: 'bi bi-chevron-down',
+            paginationSwitchUp: 'bi bi-chevron-up',
+        },
+        onPostBody,
+    };
+}
+
+$(document).ready(function () {
+    initBootstrapTable('#table-user-profile-posts', generateTableConfig('/ajax/user-posts', [
+        { field: 'title', title: 'Titre', formatter: titleFormatter, width: '35', widthUnit: '', widthUnit: '%' },
+        { field: 'category', title: 'Categorie', width: '15', widthUnit: '%' },
+        { field: 'created_at', title: 'Créé le', formatter: dateFormatter, width: '10', widthUnit: '%' },
+        { field: 'tags', title: 'Tags', formatter: tagsFormatter, width: '15', widthUnit: '%' },
+        { field: 'status', title: 'Statut', formatter: isEnabledFormatter, width: '10', widthUnit: '%' },
+        { field: 'post.slug', title: 'Actions', formatter: actionFormatter, width: '15', widthUnit: '%' },
+    ], function () {
+        var table = $('#table-user-profile-posts');
+        var page = table.bootstrapTable('getOptions').pageNumber;
+        var limit = table.bootstrapTable('getOptions').pageSize;
+        updateBootstrapTableOptions(page, limit);
+    }));
+
+    initBootstrapTable('#table-user-profile-comments', generateTableConfig('/ajax/user-comments', [
+        { field: 'post_title', title: 'Post', formatter: titleFormatter, width: '15', widthUnit: '%' },
+        { field: 'content', title: 'Contenu', formatter: contentPreviewFormatter, width: '55', widthUnit: '%' },
+        { field: 'created_at', title: 'Créé le', formatter: dateFormatter, width: '10', widthUnit: '%' },
+        { field: 'is_enabled', title: 'Statut', formatter: isEnabledFormatter, width: '10', widthUnit: '%' },
+        { field: 'post.slug', title: 'Actions', formatter: actionFormatter, width: '10', widthUnit: '%' },
+    ], function () {
+        var table = $('#table-user-profile-comments');
+        var page = table.bootstrapTable('getOptions').pageNumber;
+        var limit = table.bootstrapTable('getOptions').pageSize;
+        updateBootstrapTableOptions(page, limit);
+    }));
+
+    initBootstrapTable('#table-all-comments', generateTableConfig('/ajax/user-comments', [
+        { field: 'post_title', title: 'Post', formatter: titleFormatter, width: '15', widthUnit: '%' },
+        { field: 'content', title: 'Contenu', formatter: contentPreviewFormatter, width: '55', widthUnit: '%' },
+        { field: 'created_at', title: 'Créé le', formatter: dateFormatter, width: '10', widthUnit: '%' },
+        { field: 'is_enabled', title: 'Statut', formatter: isEnabledFormatter, width: '10', widthUnit: '%' },
+        { field: 'user', title: 'Utilisateur', formatter: userFormatter, width: '10', widthUnit: '%' },
+        { field: 'post.slug', title: 'Actions', formatter: actionFormatter, width: '10', widthUnit: '%' },
+    ], function () {
+        var table = $('#table-user-profile-comments');
+        var page = table.bootstrapTable('getOptions').pageNumber;
+        var limit = table.bootstrapTable('getOptions').pageSize;
+        updateBootstrapTableOptions(page, limit);
+    }));
+
+    toggleCardView();
+    window.addEventListener('resize', toggleCardView);
+});
