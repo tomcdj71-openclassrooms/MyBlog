4622cb573aaa90c6366c1cef9bc39265f8842386
tomcdj71 <contact.tomc@yahoo.fr>
2023-05-08 09:17:34 +0200

    style: fix layout display

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -69,41 +69,6 @@
     }
 
     /**
-     * Back to top button
-     */
-    let backtotop = select('.back-to-top')
-    if (backtotop) {
-        const toggleBacktotop = () => {
-            if (window.scrollY > 100) {
-                backtotop.classList.add('active')
-            } else {
-                backtotop.classList.remove('active')
-            }
-        }
-        window.addEventListener('load', toggleBacktotop)
-        onscroll(document, toggleBacktotop)
-    }
-
-    /**
-     * Mobile nav toggle
-     */
-    on('click', '.mobile-nav-toggle', function (e) {
-        select('#navbar').classList.toggle('navbar-mobile')
-        this.classList.toggle('bi-list')
-        this.classList.toggle('bi-x')
-    })
-
-    /**
-     * Mobile nav dropdowns activate
-     */
-    on('click', '.navbar .dropdown > a', function (e) {
-        if (select('#navbar').classList.contains('navbar-mobile')) {
-            e.preventDefault()
-            this.nextElementSibling.classList.toggle('dropdown-active')
-        }
-    }, true)
-
-    /**
      * Scrool with ofset on links with a class name .scrollto
      */
     on('click', '.scrollto', function (e) {
@@ -145,15 +110,4 @@
             backDelay: 2000
         });
     }
-
-    /**
-     * Preloader
-     */
-    let preloader = select('#preloader');
-    if (preloader) {
-        window.addEventListener('load', () => {
-            preloader.remove()
-        });
-    }
-
 })();
