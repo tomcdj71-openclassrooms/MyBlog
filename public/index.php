499d0dbdad73ae16610913132739ef6b35dda36c
tomcdj71 <contact.tomc@yahoo.fr>
2023-05-23 13:01:20 +0200

    dev: disable debugger
    Debugger is still ready to be enabled, but commented
    
    please, install devDependencies to use it

--- public/index.php
+++ public/index.php
@@ -4,20 +4,31 @@ declare(strict_types=1);
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+use App\Config\Configuration;
 use App\Controller\ErrorController;
 use App\DependencyInjection\Container;
 use App\DependencyInjection\Injectable;
 use App\Router\HttpException;
 use App\Router\Router;
-use Tracy\Debugger;
+
+/**
+ * As it is a dev dependency, it is not available in production environment
+ * To install it, run the following command:
+ * composer require --dev tracy/tracy
+ * Then uncomment the following line.
+ */
+// use Tracy\Debugger;
 
 // Create the container
 $container = new Container();
 
-Debugger::enable();
-
 // Register the services
 Injectable::register($container);
+
+// Enable Tracy for debugging
+// if ('dev' === $container->get(Configuration::class)->get('mode')) {
+//    Debugger::enable();
+// }
 
 try {
     $router = new Router($_SERVER['REQUEST_URI'], $container);
